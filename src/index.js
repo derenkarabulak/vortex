22b37adf53c1a05aa3249c305f49feea8ce36f75
Deren Karabulak <derenkarabulak@gmail.com>
2024-05-16 22:21:58 +0300

    oo

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <ToastContainer
         theme="dark"
         position="top-right"
